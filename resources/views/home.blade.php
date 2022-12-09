dfc5198e5225582a86b6400d3308c76d17e662ab
Yochan7478 <flying.for.eternity.8747@gmail.com>
2022-12-09 21:08:31 +0900

    20221209YK

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You is logged in!') }}
                 </div>
             </div>
         </div>
